93dd6904a77a1ba4a8727db0672a4e31123f2bcb
Bw <73044701+FerretFeet@users.noreply.github.com>
2025-01-18 16:42:47 -0600

    added globals.css and minor adjustments to ui components

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import {
   Links,
   Meta,
@@ -5,6 +7,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import './globals.css'
 
 export default function App() {
   return (
@@ -12,6 +15,13 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
         <Meta />
         <Links />
       </head>
