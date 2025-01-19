128e4f8c8d92d147beafba2ca4c802230255ecad
Bw <73044701+FerretFeet@users.noreply.github.com>
2025-01-19 17:38:02 -0600

    about us styled except img

--- app/ui_components/sections/simpleIHP/SimpleIHP.tsx
+++ app/ui_components/sections/simpleIHP/SimpleIHP.tsx
@@ -1,15 +1,17 @@
 // Simple Image-Header-Paragraph Section
 
 import ImgNText from "~/interfaces/ImgNText";
+import styles from "./SimpleIHP.module.css"
+
 
 export default function SimpleIHP({imgLink, sectionTitle, pText}: {imgLink: ImgNText, sectionTitle: string, pText: string}) {
     return (
-        <section className="sectionContainer">
-            <div className="imgContainer">
-                <img src={imgLink.imgLink} alt={imgLink.imgAltText} className="sectionImg" />
+        <section className={styles.sectionContainer}>
+            <div className={styles.imgContainer}>
+                <img src={imgLink.imgLink} alt={imgLink.imgAltText} className={styles.sectionImg} />
             </div>
-            <h3 className="sectionTitle">{sectionTitle}</h3>
-            <p className="paragraph" dangerouslySetInnerHTML={{__html: pText}}></p>
+            <h3 className={styles.sectionTitle}>{sectionTitle}</h3>
+            <p className={styles.paragraph} dangerouslySetInnerHTML={{__html: pText}}></p>
   
         </section>
     )
