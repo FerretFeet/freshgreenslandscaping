6beb48d70ea7c82439ba20307c91b7536e2fb32c
Bw <73044701+FerretFeet@users.noreply.github.com>
2025-01-19 15:32:38 -0600

    service areas section styled

--- app/ui_components/sections/serviceArea/ServiceArea.tsx
+++ app/ui_components/sections/serviceArea/ServiceArea.tsx
@@ -1,19 +1,27 @@
 import ImgNText from "~/interfaces/ImgNText";
+import styles from "./ServiceArea.module.css"
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function ServiceArea({stateImg, serviceAreas}: {stateImg: ImgNText, serviceAreas: Array<string>}) {
     return (
-        <section className="container">
-            <img src={stateImg.imgLink} alt={stateImg.imgAltText} className="stateImg" />
-            <h2 className="title">Service Areas</h2>
-            <ul className="list">
-                {serviceAreas.map((area, idx) => (
-                    <li className="serviceAreaItem" key={idx}>
-                        <img src="../../../../public/imgs/icons/mapPin.png" alt="" className="areaIcon" />
-                        {area}
-                    </li>
-                ))}
-            </ul>
+        <div className={styles.hideOverflow}>
+            <div className={styles.stylingContainer}>
+                <section className={styles.container}>
+                    {/* <img src={stateImg.imgLink} alt={stateImg.imgAltText} className={styles.stateImg} /> */}
+                    <h2 className={styles.title}>Service Areas</h2>
+                    <ul className={styles.list}>
+                        {serviceAreas.map((area, idx) => (
+                            <li className={styles.serviceAreaItem} key={idx}>
+                                <img src="../../../../public/imgs/icons/mapPin.png" alt="" className={styles.areaIcon} />
+                                {area}
+                            </li>
+                        ))}
+                    </ul>
+                </section>
+            </div>
+        </div>
 
-        </section>
     )
+
+
 }
