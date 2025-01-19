575ec519ccbe6afeb59c8e8f6b8ffdfe6cfd46e5
Bw <73044701+FerretFeet@users.noreply.github.com>
2025-01-19 09:54:24 -0600

    replaced hard-coded values with variables in header section

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -4,6 +4,8 @@ import data from '../../data/valuePropositions.json';
 import SimpleImgCard from "~/ui_components/elements/simple_img_card/SimpleImageCard";
 import SimpleIconCard2 from "~/ui_components/elements/simple_icon_card_2/SimpleIconCard2";
 import Header from "~/ui_components/sections/header/Header";
+import links from "~/data/links.json"
+import { LinkString } from "~/interfaces/LinkString";
 
 
 export const meta: MetaFunction = () => {
@@ -14,9 +16,14 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const quoteFormLink: LinkString = {key: "Get An Estimate", value: links["Get An Estimate"]};
+  const contactLink: LinkString = {key: "Contact Us", value: links["Contact Us"]};
+  const contactPhone: LinkString = {key: "Phone", value: links.Phone}
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <Header />
+      <Header quoteFormLink={quoteFormLink} contactLink={contactLink} contactPhone={contactPhone} logoPath="../../../public/imgs/logo.png"
+        navLinks={links} excludedNavLinkKeys={["Get An Estimate", "Contact Us", "Phone"]}/>
       <SimpleIconCard2 title={data[4].title} description={data[4].description} iconLink={data[4].iconLink} />
 
             <SimpleImgCard title={data[2].title} description={data[2].description} iconLink={data[2].iconLink} />
--- app/ui_components/sections/header/Header.tsx
+++ app/ui_components/sections/header/Header.tsx
@@ -1,23 +1,35 @@
 import styles from './Header.module.css'
-import links from '../../../data/links.json'
 import createLinksAndNav from '~/utility/createLinksAndNav'
 import { Link } from '@remix-run/react'
+import { LinkString } from '~/interfaces/LinkString'
 
 
-export default function Header() {
+export default function Header({quoteFormLink, contactLink, contactPhone, logoPath, navLinks, excludedNavLinkKeys} : {quoteFormLink: LinkString, contactLink: LinkString, contactPhone: LinkString, logoPath: string, navLinks: LinkString, excludedNavLinkKeys: Array<string>}) {
+// NOTE:: CONTAINS HARD PATH TO LOGO PICTURE
 
+    const trimPhoneLink = (phoneLink: string) => {
+    // Assume given tel:+{11-Digit-Number} && First digit not needed
+    // Add dashes between phone number sections
+    const slicedPhone = phoneLink.slice(6)
+    
+    const areaCode = slicedPhone.slice(0,3);
+    const prefix = slicedPhone.slice(3,6);
+    const lineNumber = slicedPhone.slice(6);
+    return `${areaCode}-${prefix}-${lineNumber}`
+    
+}
 return (
     <div className={styles.container}>
         <div className={styles.contactBanner}>
-            <Link to={links['Quote']}>
-                <button type="button" className={styles.quoteBtn}>Get A Quote</button>
+            <Link to={Object.values(quoteFormLink)[1]}>
+                <button type="button" className={styles.quoteBtn}>{Object.values(quoteFormLink)[0]}</button>
             </Link>
-            <Link to={links['Contact Us']} className={styles.contactLink}>Contact Us</Link>
-            <a href={links['Phone']}>501-510-3469</a>
+            <Link to={Object.values(contactLink)[1]} className={styles.contactLink}>{Object.values(contactLink)[0]}</Link>
+            <a href={Object.values(contactPhone)[1]}>{trimPhoneLink(Object.values(contactPhone)[1])}</a>
         </div>
         <div className={styles.logoNav}>
-            <img src="../../../../public/imgs/logo.png" alt="logo" className={styles.logo} />
-            {createLinksAndNav(links, ['Contact Us', 'Phone', 'Quote'])}
+            <img src={logoPath} alt="logo" className={styles.logo} />
+            {createLinksAndNav(navLinks, excludedNavLinkKeys)}
         </div>
     </div>
 )
