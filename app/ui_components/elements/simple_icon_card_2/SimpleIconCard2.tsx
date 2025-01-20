1933facbb4223e0130a07cd9744bb9eb6c03c6e2
Bw <73044701+FerretFeet@users.noreply.github.com>
2025-01-20 17:27:38 -0600

    modified testimonials and simpleiconcard styles

--- app/ui_components/elements/simple_icon_card_2/SimpleIconCard2.tsx
+++ app/ui_components/elements/simple_icon_card_2/SimpleIconCard2.tsx
@@ -8,8 +8,11 @@ export default function SimpleIconCard({title, description, iconLink} : {title: 
                 {/* svg or img */}
                 <img src={iconLink.imgLink} alt={iconLink.imgAltText} className={styles.iconImg} />
             </div>
-            <p className={styles.description}>{description}</p>
-            <h6 className={styles.title}>{title}</h6>
+            <div className={styles.textStyler}>
+                <p className={styles.description}>{description}</p>
+                <h6 className={styles.title}>{title}</h6>
+            </div>
+
 
         </div>
     )
