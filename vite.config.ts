2e89ab5993b10ba0264c9e28a02be2af17ccfe5e
Bw <73044701+FerretFeet@users.noreply.github.com>
2025-02-03 14:43:35 -0600

    trying to test deploy to netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { netlifyPlugin } from "@netlify/remix-edge-adapter/plugin";
+import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 
 export default defineConfig({
   plugins: [remix(), netlifyPlugin(), tsconfigPaths()],
   ssr: {
-    noExternal: ['node:crypto']
   }
 });
