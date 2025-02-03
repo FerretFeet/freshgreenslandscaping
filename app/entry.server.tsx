b5204f726da3150f25ccbf768aa677f4bdaa880a
Bw <73044701+FerretFeet@users.noreply.github.com>
2025-02-03 14:58:26 -0600

    commented entry.server.tsx

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,3 +1,4 @@
-// @ts-expect-error virtual module
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore virtual module
 // eslint-disable-next-line import/no-unresolved
-export { default } from "virtual:netlify-server-entry";
+// export { default } from "virtual:netlify-server-entry";
