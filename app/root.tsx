4a3bd1db5dc324ffb8f7e1a800690c6df1a0f680
Bw <73044701+FerretFeet@users.noreply.github.com>
2025-04-06 11:32:19 -0500

    added ahrefs script to root head

--- app/root.tsx
+++ app/root.tsx
@@ -7,17 +7,32 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import './globals.css'
+import "./globals.css";
 
 export default function App() {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
+        <link
+          rel="preconnect"
+          href="https://fonts.googleapis.com"
+        />
+        <script
+          src="https://analytics.ahrefs.com/analytics.js"
+          data-key="iB1d8xD49yUIkkwYyN/d8g"
+          async
+        ></script>
+
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,55 +1,82 @@
 import type { MetaFunction } from "@netlify/remix-runtime";
 import Header from "~/ui_components/sections/header/Header";
-import links from "~/data/links.json"
+import links from "~/data/links.json";
 import { DataString } from "~/interfaces/DataString";
 import Hero from "~/ui_components/sections/hero/Hero";
 import LinkButton from "~/ui_components/elements/link_button/LinkButton";
 import heroTxt from "~/data/homeHeroTxt.json";
 import ValueProps from "~/ui_components/sections/valueProps/ValueProps";
-import valuePropData from "~/data/valuePropositions.json"
+import valuePropData from "~/data/valuePropositions.json";
 import ServiceArea from "~/ui_components/sections/serviceArea/ServiceArea";
-import serviceAreas from "~/data/serviceAreas.json"
+import serviceAreas from "~/data/serviceAreas.json";
 import ServiceCards from "~/ui_components/sections/serviceCards/ServiceCards";
-import serviceCardData from "~/data/serviceCards.json"
+import serviceCardData from "~/data/serviceCards.json";
 import SimpleIHP from "~/ui_components/sections/simpleIHP/SimpleIHP";
-import aboutUsTxt from "~/data/aboutUsTxt.json"
-import FormConstructor, { formAction } from "~/ui_components/base/formConstructor.tsx/FormConstructor";
+import aboutUsTxt from "~/data/aboutUsTxt.json";
+import FormConstructor, {
+  formAction,
+} from "~/ui_components/base/formConstructor.tsx/FormConstructor";
 import Testimonials from "~/ui_components/sections/testimonials/Testimonials";
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+// export const meta: MetaFunction = () => {
+//   return [
+//     { title: "New Remix App" },
+//     { name: "description", content: "Welcome to Remix!" },
+//   ];
+// };
 
-export const action = formAction
+export const action = formAction;
 
 export default function Index() {
-  const quoteFormLink: DataString = {key: "Get An Estimate", value: links["Get An Estimate"]};
-  const contactLink: DataString = {key: "Contact Us", value: links["Contact Us"]};
-  const contactPhone: DataString = {key: "Phone", value: links.Phone}
-
+  const quoteFormLink: DataString = {
+    key: "Get An Estimate",
+    value: links["Get An Estimate"],
+  };
+  const contactLink: DataString = {
+    key: "Contact Us",
+    value: links["Contact Us"],
+  };
+  const contactPhone: DataString = { key: "Phone", value: links.Phone };
 
   const MyCTAModule = () => (
     // Used in Hero
-    <LinkButton link={Object.values(quoteFormLink)[1]} text={Object.values(quoteFormLink)[0]}/>
-      
+    <LinkButton
+      link={Object.values(quoteFormLink)[1]}
+      text={Object.values(quoteFormLink)[0]}
+    />
   );
-
-  
 
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <Header quoteFormLink={quoteFormLink} contactLink={contactLink} contactPhone={contactPhone} logoPath="/imgs/icons/logo.png"
-        navLinks={links} excludedNavLinkKeys={["Get An Estimate", "Contact Us", "Phone"]}/>
-        <Hero h1text={heroTxt.h1text} description={heroTxt.description} CTAModule={MyCTAModule}/>
-        <ValueProps cardInfoArr={valuePropData}/>
-        <ServiceArea stateImg={{imgLink: '../../../public/imgs/icons/map1.png', imgAltText: 'The State of Arkansas'}} serviceAreas={serviceAreas}/>
-        <ServiceCards cardInfoArr={serviceCardData} />
-        <Testimonials cardInfoArr={valuePropData} />
-        <SimpleIHP imgLink={{imgLink: '/', imgAltText: 'a photo'}} sectionTitle= 'About Us' pText={aboutUsTxt.paragraphText} />
-        <FormConstructor />
+      <Header
+        quoteFormLink={quoteFormLink}
+        contactLink={contactLink}
+        contactPhone={contactPhone}
+        logoPath="/imgs/icons/logo.png"
+        navLinks={links}
+        excludedNavLinkKeys={["Get An Estimate", "Contact Us", "Phone"]}
+      />
+      <Hero
+        h1text={heroTxt.h1text}
+        description={heroTxt.description}
+        CTAModule={MyCTAModule}
+      />
+      <ValueProps cardInfoArr={valuePropData} />
+      <ServiceArea
+        stateImg={{
+          imgLink: "../../../public/imgs/icons/map1.png",
+          imgAltText: "The State of Arkansas",
+        }}
+        serviceAreas={serviceAreas}
+      />
+      <ServiceCards cardInfoArr={serviceCardData} />
+      <Testimonials cardInfoArr={valuePropData} />
+      <SimpleIHP
+        imgLink={{ imgLink: "/", imgAltText: "a photo" }}
+        sectionTitle="About Us"
+        pText={aboutUsTxt.paragraphText}
+      />
+      <FormConstructor />
     </div>
   );
 }
